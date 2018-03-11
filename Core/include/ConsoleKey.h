d92149ef5fd32ae8eadc1375caa8524425f8f9ca
trekkiegod <trekkiegod@gmail.com>
2018-03-11 10:46:20 -0400

    Comments for console classes

--- Core/include/ConsoleKey.h
+++ Core/include/ConsoleKey.h
@@ -1,7 +1,24 @@
+/***********************************************************************************************************************
+* @file ConsoleKey.h
+* @author Rod Leonard
+* @version 0.0.1
+* @date 2018/03/11
+* @copyright Dynabyte Software LLC, licensed under LGPL 3.0 so later
+* @brief Header file for DynabyteSoftware's ConsoleKey enumeration
+**********************************************************************************************************************/
+
 #pragma once
 
 namespace DynabyteSoftware
 {
+  /********************************************************************************************************************
+  * @enum ConsoleKey
+  * @ingroup DynabyteSoftware
+  * @brief Enumeration describing keyboard keys
+  * @details
+  * An enumeration describing which keyboard key was pressed, independently of modifiers that affect
+  * its character
+  ********************************************************************************************************************/
   enum struct ConsoleKey : char
   {
     #pragma region Upper Case Letters
